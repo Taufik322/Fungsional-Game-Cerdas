47d3bafc20174a04af8a30a1f2e9855809140ec7
Ajengcp <ajengcp@webmail.umm.ac.id>
2021-12-26 23:56:10 +0700

    papan skor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+from operator import getitem
 
 users = {
     1: {
@@ -22,6 +24,42 @@ def quiz():
 
 def suit():
     print("Ini fungsi suit")
+
+def scores():
+    # ranks = sorted(users.items(), key=lambda x: x[1], reverse=True)
+    # ranks = sorted(users.items(), lambda x: x[0], key=lambda x: x[1])
+    #
+    # for i in range(len(ranks)):
+    #     print(ranks["Point"][i])
+
+    # ranks = sorted(users, key = lambda i: i['point'])
+
+    # ranks = sorted(users, key = lambda x: x[2]['point'])
+    ranks = list(sorted(users.items(), key = lambda x: getitem(x[1], 'point'), reverse=True))
+    # users[sortir]["username"]
+    # ranks = sorted(users, lambda  x: x[1]['point'] == 0)
+    # for i in range(len(users)):
+
+
+
+    # for i in range(len(users)):
+    #     i += 1
+    #     if ranks.values() == '':
+    #         ranks.update(users[i])
+    #     elif i == len(users):
+    #         break
+    #     elif users[i+1]["point"] > users[i]["point"]:
+    #         ranks.update(users[i])
+
+    # print(str(ranks[0][1]['point']))
+    # print(str(ranks[1][1]['point']))
+    # print(str(ranks[2][1]['point']))
+    print(f'{"="*7}PAPAN SKOR{"="*7}')
+    for i in range(len(ranks)):
+        print(f'{i+1}. {str(ranks[i][1]["username"])}        {str(ranks[i][1]["point"])}' )
+
+
+
 
 def login():
     print(f'\n{"="*7}PEMINJAMAN dan PEMBELIAN KENDARAAN{"="*7}')
@@ -51,13 +89,16 @@ def dashboard():
         print(f'{"="*7}Selamat Datang Di Hagi!{"="*7}')
         print("1. Kuis Pengetahuan Umum")
         print("2. Batu, Gunting, Kertas")
-        print("3. Logout")
+        print("3. Users Scores")
+        print("4. Logout")
         inputs = input("Pilih: ")
         if inputs == '1':
             quiz()
         elif inputs == '2':
             suit()
         elif inputs == '3':
+            scores()
+        elif inputs == '4':
             login()
 
 
